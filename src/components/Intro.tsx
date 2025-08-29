8534295cce77bd6947b4832a61eabed2c0900a93
Abeer Kapoor <abrkapoor@gmail.com>
2025-08-29 10:59:30 +0530

    Add PDF import for CV download link in Intro component

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -2,6 +2,8 @@ import profilePic from "../assets/illustrations/profile-picture.png";
 import personalInfo from "../data/personal_info.json";
 import { motion } from "framer-motion";
 import { FaLinkedin, FaGithub, FaXTwitter, FaDownload } from "react-icons/fa6";
+import pdfFile from 'src/assets/ABR-CV25-11.pdf';
+
 
 const Intro = () => {
   return (
@@ -82,7 +84,7 @@ const Intro = () => {
 
           {/* Download CV */}
           <a
-              href="/src/assets/ABR-CV25-11.pdf"
+              href={pdfFile}
               download
               className="flex items-center gap-2 px-5 py-2.5 rounded-lg bg-gray-900 text-purple-400 border border-purple-700 shadow-md hover:shadow-purple-500/60 hover:bg-purple-900 hover:text-white focus:outline-none focus:ring-2 focus:ring-purple-400 transition-all duration-200 font-medium hover:drop-shadow-[0_0_12px_rgba(168,85,247,0.7)]"
               aria-label="Download CV"
